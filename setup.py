f1b417f6feb9d255614b8a3659121676106ef2cd
Gordon Brander <gordonbrander@users.noreply.github.com>
2017-01-20 13:54:30 -0800

    Load ddoc from fixture

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os import path
+
+readme_path = path.join(path.dirname(__file__), "README.md")
+with open(readme_path) as f:
+    readme = f.read()
+
+setup(
+    name='couchtools',
+    version='0.0.1',
+    author='OpenAgricultureInitiative',
+    description='Command line utility for working with CouchDB design documents',
+    long_description=readme,
+    license="GPL-3.0",
+    url="https://github.com/OpenAgInitiative/couchtools",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.7",
+    ],
+    packages=find_packages(exclude=("tests", "tests.*")),
+    install_requires=[
+        "CouchDB>=1.0"
+    ],
+    extras_require={},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'couchtools_load_fixture=couchtools:cli_load_fixture'
+        ]
+    }
+)
